224b23a1ed9bb237ab71bc7eaefb9bb68afbfad1
nati <nwtbekele@gmail.com>
2025-01-05 21:37:35 -0600

    install script

---
+++ install_req.sh
@@ -0,0 +1,10 @@
+while read line; do
+  if [[ "$line" == *"madmom"* ]]; then
+    madmom_version="$line"
+  else
+    pip install $line
+  fi
+done < requirements.txt
+
+pip install "$madmom_version"
+
